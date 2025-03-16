b13cca27a6a4370adc32e8ef310110bad4723f7d
Pratik <pgumthanavar05@gmail.com>
2025-03-16 17:49:16 +0530

    keeping the redux store an session store in sync.

--- src/redux/appStore.jsx
+++ src/redux/appStore.jsx
@@ -1,10 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import noteReducer from './noteSlice';
 
+const getSavedNotes = () => {
+  const sessionData = sessionStorage.getItem('savedNotes');
+  return sessionData ? JSON.parse(sessionData) : undefined;
+};
+
+const saveNotes = (state) => {
+  const reduxStateData = JSON.stringify(state);
+  sessionStorage.setItem('savedNotes', reduxStateData);
+};
+
+const retrieveNotes = getSavedNotes();
+
 const appStore = configureStore({
   reducer: {
     note: noteReducer,
   },
+  preloadedState: retrieveNotes,
+});
+
+appStore.subscribe(() => {
+  saveNotes(appStore.getState());
 });
 
 export default appStore;
